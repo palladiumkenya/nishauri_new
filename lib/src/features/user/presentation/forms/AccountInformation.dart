ff850000314be3c83914a600293bbc1eb430b4bd
Laurent Ouma <lawiomosh3@gmail.com>
2023-11-05 19:37:56 +0300

    Accountinfoof profile update form implemented and works fine

--- lib/src/features/user/presentation/forms/AccountInformation.dart
+++ lib/src/features/user/presentation/forms/AccountInformation.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nishauri/src/shared/input/FormInputTextField.dart';
+import 'package:nishauri/src/utils/constants.dart';
 
 class AccountInformation extends StatelessWidget {
   final TextEditingController username;
@@ -13,6 +15,53 @@ class AccountInformation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Column(
+        children: [
+          const SizedBox(height: Constants.SPACING),
+          FormInputTextField(
+            controler: username,
+            placeholder: "Enter your username",
+            prefixIcon: Icons.account_circle_outlined,
+            label: "Username",
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: Constants.SPACING),
+          FormInputTextField(
+            controler: email,
+            keyboardType: TextInputType.emailAddress,
+            placeholder: "Enter your email",
+            prefixIcon: Icons.email,
+            label: "Email",
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: Constants.SPACING),
+          FormInputTextField(
+            controler: phoneNumber,
+            keyboardType: TextInputType.phone,
+            placeholder: "Enter your phone number",
+            prefixIcon: Icons.phone,
+            label: "Phone number",
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: Constants.SPACING),
+        ],
+      ),
+    );
   }
 }
