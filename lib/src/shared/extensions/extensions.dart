34c003c8493661fea0fee0292463be488f4d661c
Omoshlawi <lawiomosh3@gmail.com>
2023-11-30 00:30:59 +0300

    Fixed stubborn string extension bug that caused error

--- lib/src/shared/extensions/extensions.dart
+++ lib/src/shared/extensions/extensions.dart
@@ -3,13 +3,22 @@ import 'dart:core';
 
 extension ExtendedString on String {
   String get titleCase {
+    if (this.isEmpty) return ''; // Handle empty string
     final words = this.split(' ');
     // Capitalize the first letter of each word and join them back
-    return words.map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase()).join(' ');
+    return words.map((word) {
+      if (word.isEmpty) return '';
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    }).join(' ');
   }
+
   String get abbreviation {
+    if (this.isEmpty) return ''; // Handle empty string
     final words = this.split(' ');
     // Capitalize the first letter of each word and join them back
-    return words.map((word) => word[0]).join(' ').toUpperCase();
+    return words.map((word) {
+      if (word.isEmpty) return '';
+      return word[0];
+    }).join(' ').toUpperCase();
   }
 }
