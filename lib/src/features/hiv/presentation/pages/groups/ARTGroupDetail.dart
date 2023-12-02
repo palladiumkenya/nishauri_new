b63399b5a9a86eb86ac12f7b57b27a4be3ee1173
Omoshlawi <lawiomosh3@gmail.com>
2023-12-02 21:01:31 +0300

    ART Groups screen details implemented and looks greate

--- lib/src/features/hiv/presentation/pages/groups/ARTGroupDetail.dart
+++ lib/src/features/hiv/presentation/pages/groups/ARTGroupDetail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nishauri/src/features/hiv/data/providers/art_group_provider.dart';
+import 'package:nishauri/src/shared/display/AppCard.dart';
 import 'package:nishauri/src/shared/display/ProfileCard.dart';
 import 'package:nishauri/src/utils/constants.dart';
 
@@ -28,24 +29,94 @@ class ARTGroupDetailScreen extends StatelessWidget {
             data: (artGroups) {
               final artGroup =
                   artGroups.where((element) => element.id == groupId).first;
-              return ListView(
-                children: [
-                  ProfileCard(),
+              return ProfileCard(
+                coverPhoto: "https://picsum.photos/seed/picsum/200/300",
+                buildItem: (context, item) => item,
+                height: MediaQuery.of(context).size.height,
+                header: Column(
+                  children: [
+                    Center(
+                      child: Text(artGroup.group.title,
+                          style: theme.textTheme.headlineMedium),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (artGroup.isCurrent == true)
+                          Container(
+                            margin: const EdgeInsets.all(Constants.SPACING),
+                            padding: const EdgeInsets.all(Constants.SPACING),
+                            decoration: BoxDecoration(
+                                color:
+                                    theme.colorScheme.primary.withOpacity(0.1),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(Constants.ROUNDNESS),
+                                )),
+                            child: const Text("Community ART Model"),
+                          ),
+                        Container(
+                          margin: const EdgeInsets.all(Constants.SPACING),
+                          padding: const EdgeInsets.all(Constants.SPACING),
+                          decoration: BoxDecoration(
+                              color: theme.colorScheme.primary.withOpacity(0.1),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(Constants.ROUNDNESS),
+                              )),
+                          child: const Text("\t\tCurrent\t\t"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                // bodyContainerDecoration: BoxDecoration(color: theme.colorScheme.secondary),
+                items: [
+                  const Divider(),
                   ListTile(
-                    title: const Text("Title"),
-                    subtitle: Text(artGroup.group.title),
-                    leading: const CircleAvatar(child: Icon(Icons.group)),
+                    leading: const CircleAvatar(child: Icon(Icons.person)),
+                    title: const Text("Lead"),
+                    subtitle: const Text("Omondi Laurent Ouma"),
+                    trailing: OutlinedButton(
+                      onPressed: () {},
+                      child: const Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        spacing: 5,
+                        children: [
+                          Icon(Icons.phone),
+                          Text("0793889658"),
+                        ],
+                      ),
+                    ),
                   ),
+                  const Divider(),
                   const ListTile(
-                    leading: CircleAvatar(child: Icon(Icons.person)),
-                    title: Text("Lead"),
-                    subtitle: Text("Omondi Laurent Ouma"),
+                    leading: CircleAvatar(child: Icon(Icons.subscriptions)),
+                    title: Text("Subscribers"),
+                    subtitle: Text("12 Members"),
                   ),
+                  const Divider(),
                   const ListTile(
-                    leading: CircleAvatar(child: Icon(Icons.person)),
-                    title: Text("Lead"),
-                    subtitle: Text("Omondi Laurent Ouma"),
+                    leading: CircleAvatar(child: Icon(Icons.subscriptions)),
+                    title: Text("Offline Subscribers"),
+                    subtitle: Text("4 Members"),
                   ),
+                  const Divider(),
+                  const ExpansionTile(
+
+                    title: Text("Group description"),
+                    leading: CircleAvatar(child: Icon(Icons.info)),
+                    subtitle: Text("Brief descriptions truncated ...."),
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(Constants.SPACING),
+                        child: Text("""What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type 
+specimen book. It has survived not only five centuries, but also the leap  """),
+                      ),
+                    ],
+                  ),
+                  const Divider(),
                 ],
               );
             },
--- lib/src/shared/display/ProfileCard.dart
+++ lib/src/shared/display/ProfileCard.dart
@@ -3,62 +3,85 @@ import 'package:nishauri/src/utils/constants.dart';
 
 class ProfileCard extends StatelessWidget {
   final double height;
+  final String? coverPhoto;
+  final Widget Function(BuildContext, dynamic)? buildItem;
+  final List<dynamic> items;
+  final BoxDecoration? bodyContainerDecoration;
+  final EdgeInsetsGeometry? bodyContainerPadding;
+  final EdgeInsetsGeometry? bodyContainerMargin;
+  final Widget? header;
+  final double headerFactor;
 
-  const ProfileCard({super.key, this.height = 220});
+  const ProfileCard({super.key,
+    this.height = 600,
+    this.coverPhoto,
+    this.buildItem,
+    this.items = const [],
+    this.bodyContainerDecoration,
+    this.bodyContainerMargin,
+    this.bodyContainerPadding,
+  this.header,
+    this.headerFactor = 0.2
+  });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      height: height,
-      decoration: BoxDecoration(
-        color: theme.colorScheme.primary,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(Constants.ROUNDNESS),
-        ),
-      ),
-      child: Stack(
-        // alignment: Alignment.center,
-        children: [
-          Container(
-            // Cover photo
-            width: double.maxFinite,
-            // height: height,
-            // decoration: BoxDecoration(color: theme.colorScheme.error),
-            child: Column(children: [
-              Container(
-                height: height * 0.45,
-                width: double.maxFinite,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      "https://picsum.photos/200/300",
+    final headerHeight = height * headerFactor;
+    return Column(
+      children: [
+        Container(
+          height: headerHeight,
+          decoration: bodyContainerDecoration,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        backgroundBlendMode: BlendMode.darken,
+                        image: coverPhoto != null ? DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            coverPhoto!,
+                          ),
+                        ):null,
+                      ),
                     ),
                   ),
+                  const Spacer(flex: 2),
+                ],
+              ),
+              CircleAvatar(
+                radius: headerHeight * 0.42,
+                child: Icon(
+                  Icons.group,
+                  size: headerHeight * 0.3,
                 ),
               ),
-            ]),
+            ],
           ),
-          Align(
-            heightFactor: 1.6,
-            child: CircleAvatar(
-              radius: height * 0.35,
-              child: Icon(
-                Icons.group,
-                size: height * 0.3,
+        ),
+        if(header != null)
+          header!,
+        if (buildItem != null)
+          Expanded(
+            child: Container(
+              decoration: bodyContainerDecoration,
+              padding: bodyContainerPadding,
+              margin: bodyContainerMargin,
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) =>
+                    buildItem!(context, items[index]),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              "Omondi Laurent Ouma",
-              style: theme.textTheme.titleLarge,
-            ),
           )
-        ],
-      ),
+      ],
     );
   }
 }
