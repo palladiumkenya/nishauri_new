bb5f7662fd35bcb40e301a36dea4a107cec1a0d1
Omoshlawi <lawiomosh3@gmail.com>
2024-02-16 12:27:00 +0300

    Tabeed orders into all, fulfilled and pending though dont have actual orders

--- lib/src/features/hiv/presentation/pages/orders/HIVDrugOrders.dart
+++ lib/src/features/hiv/presentation/pages/orders/HIVDrugOrders.dart
@@ -1,49 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nishauri/src/shared/display/AppSearch.dart';
-import 'package:nishauri/src/utils/constants.dart';
+import 'package:nishauri/src/features/hiv/presentation/widgets/orders/AllOrders.dart';
+import 'package:nishauri/src/features/hiv/presentation/widgets/orders/FulfilledOrders.dart';
+import 'package:nishauri/src/features/hiv/presentation/widgets/orders/PendingOrders.dart';
 
 class HIVDrugOrdersScreen extends StatelessWidget {
   const HIVDrugOrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => context.pop(),
-          icon: const Icon(Icons.chevron_left),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => context.pop(),
+            icon: const Icon(Icons.chevron_left),
+          ),
+          title: const Text("ARV Drug Orders"),
+          backgroundColor: Theme.of(context).primaryColor,
+          bottom: const TabBar(tabs: [
+            Tab(icon: Icon(Icons.all_inbox), text: "All"),
+            Tab(icon: Icon(Icons.pending), text: "Pending"),
+            Tab(icon: Icon(Icons.done_all), text: "FullField"),
+          ]),
         ),
-        title: const Text("ARV Drug Orders"),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      body: FractionallySizedBox(
-        heightFactor: 1,
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(Constants.SPACING),
-              child: AppSearch(),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (BuildContext context, int currIndex) => Column(
-                  children: [
-                    const Divider(),
-                    ListTile(
-                      leading: const Icon(Icons.shopping_cart),
-                      title: Text("Order $currIndex name here"),
-                      subtitle: Text("$currIndex October 2023"),
-                      trailing: const Icon(Icons.chevron_right),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        body: const FractionallySizedBox(
+          heightFactor: 1,
+          child: TabBarView(
+            children: [
+              AllOrders(),
+              PendingOrders(),
+              FulfilledOrders(),
+            ],
+          ),
         ),
       ),
     );
