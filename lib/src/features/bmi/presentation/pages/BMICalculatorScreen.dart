90bc08d1761e811f84cbae470a249480835b11cc
Omoshlawi <lawiomosh3@gmail.com>
2024-04-22 09:21:19 +0300

    Added app bar and bmi calculator design but incomplete

--- lib/src/features/bmi/presentation/pages/BMICalculatorScreen.dart
+++ lib/src/features/bmi/presentation/pages/BMICalculatorScreen.dart
@@ -1,302 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nishauri/src/shared/display/LinkedRichText.dart';
-import 'package:nishauri/src/shared/input/Button.dart';
-import 'package:nishauri/src/shared/input/FormInputTextField.dart';
-import 'package:nishauri/src/shared/layouts/ResponsiveWidgetFormLayout.dart';
+import 'package:nishauri/src/shared/display/AppCard.dart';
+import 'package:nishauri/src/shared/display/CustomeAppBar.dart';
 import 'package:nishauri/src/utils/constants.dart';
-import 'package:nishauri/src/utils/helpers.dart';
 
-class BMICalculatorScreen extends StatefulWidget {
+class BMICalculatorScreen extends HookWidget {
   const BMICalculatorScreen({super.key});
 
   @override
-  State<BMICalculatorScreen> createState() => _BMICalculatorScreenState();
-}
-
-class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
-  final List<Map<String, String>> _heightUnitChoices = [
-    {"value": "m", "label": "Metres"},
-    {"value": "cm", "label": "Centimetres"},
-    {"value": "in", "label": "Inches"},
-    {"value": "ft", "label": "Foot"}
-  ];
-
-  final List<Map<String, String>> _weightUnitChoices = [
-    {"value": "kg", "label": "Kilograms"},
-    {"value": "g", "label": "Grams"},
-    {"value": "lb", "label": "Pounds"},
-  ];
-
-  final List<Map<String, String>> genderChoices = [
-    {"label": 'Female', 'value': "F"},
-    {"label": 'Male', 'value': "M"},
-  ];
-
-  final _formKey = GlobalKey<FormState>();
-  var weight = TextEditingController();
-  var height = TextEditingController();
-  var gender = TextEditingController();
-  var age = TextEditingController();
-
-  late String heightUnits;
-  late String weightUnits;
-  late bool isPregnant;
-  late bool _showSwitch = genderChoices.first["value"] == 'F';
-  late bool _disableForm = true;
-
-  double? bmi;
-
-  @override
-  void initState() {
-    super.initState();
-    heightUnits = _heightUnitChoices.first["value"]!;
-    weightUnits = _weightUnitChoices.first['value']!;
-    isPregnant = true;
-  }
-
-  @override
-  void dispose() {
-    weight.dispose();
-    height.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        void handleSubmit() async {
-          if (_formKey.currentState!.validate()) {
-            _formKey.currentState!.save();
-            final _age = int.tryParse(age.text) ?? 0;
-
-            if (_age < 5 || isPregnant) {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  title: const Text("Warning!"),
-                  content: const Text(
-                    "Cant calculate BMI for person under age of 5 and for pregnant women",
-                  ),
-                  actions: [
-                    TextButton(
-                        onPressed: () {
-                          context.pop();
-                        },
-                        child: const Text("Ok")),
-                  ],
-                ),
-              );
-              return;
-            }
-            setState(() {
-              bmi = calculateBMI(
-                  heightUnits,
-                  weightUnits,
-                  double.tryParse(weight.text) ?? 0.0,
-                  double.tryParse(height.text) ?? 0.0);
-            });
-          }
-        }
-
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text("Calculator"),
-            leading: IconButton(
-              onPressed: () => context.pop(),
-              icon: const Icon(Icons.chevron_left),
-            ),
-          ),
-          body: ResponsiveWidgetFormLayout(
-            buildPageContent: (BuildContext context, Color? color) => SafeArea(
-              child: Container(
-                padding: const EdgeInsets.all(Constants.SPACING * 2),
-                decoration: BoxDecoration(
-                  color: color,
-                  borderRadius: BorderRadius.circular(Constants.ROUNDNESS),
-                ),
-                child: Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          const CustomAppBar(
+              title: "BMI Calculator",
+              subTitle: "Empower Your Health Journey \nWith BMI Insights",
+              icon: Icons.calculate_outlined),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(Constants.SPACING),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Choose your gender",
+                      style: theme.textTheme.titleMedium,
                     ),
-                    child: Column(
+                    const SizedBox(height: Constants.SPACING),
+                    const Row(
                       children: [
-                        const SizedBox(height: Constants.SPACING),
-                        DecoratedBox(
-                          decoration: const BoxDecoration(),
-                          child: Image.asset(
-                            "assets/images/bmi_scale.png",
-                            fit: BoxFit.contain,
-                            // height: 200,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        const Text(
-                          "BMI Calculator",
-                          style: TextStyle(
-                              fontSize: 40, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: Constants.SPACING),
-                        const Text(
-                          "This tool calculates you body-mass index\n",
-                          style: TextStyle(color: Colors.black26),
-                        ),
-                        const SizedBox(height: Constants.SPACING),
-                        Row(
-                          children: [
-                            Flexible(
-                              child: FormInputTextField(
-                                controler: age,
-                                keyboardType: TextInputType.number,
-                                placeholder: "e.g 28",
-                                prefixIcon: Icons.speed,
-                                label: "Age",
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please provide age';
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                            const SizedBox(width: Constants.SPACING),
-                            DropdownMenu<String>(
-                              label: const Text("Gender"),
-                              leadingIcon: const IconButton(
-                                onPressed: null,
-                                icon: Icon(Icons.accessibility_outlined),
-                              ),
-                              initialSelection: genderChoices.first["value"],
-                              controller: gender,
-                              onSelected: (value) {
-                                setState(() {
-                                  _showSwitch = gender.text == 'Female';
-                                });
-                              },
-                              dropdownMenuEntries: genderChoices
-                                  .map<DropdownMenuEntry<String>>(
-                                    (Map<String, String> gender) =>
-                                        DropdownMenuEntry<String>(
-                                      value: gender['value']!,
-                                      label: gender['label']!,
-                                    ),
-                                  )
-                                  .toList(),
-                            ),
-                          ],
-                        ),
-                        if (_showSwitch)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                        AppCard(
+                          child: Column(
                             children: [
-                              const Text("Pregnant: "),
-                              Switch(
-                                value: isPregnant,
-                                onChanged: (bool value) {
-                                  setState(() {
-                                    isPregnant = !isPregnant;
-                                  });
-                                },
-                              ),
+                              Icon(Icons.male),
+                              Text("Male"),
                             ],
                           ),
-                        const SizedBox(height: Constants.SPACING),
-                        FormInputTextField(
-                          keyboardType: TextInputType.number,
-                          controler: weight,
-                          placeholder: "e.g 60.0",
-                          prefixIcon: Icons.monitor_weight,
-                          label: "Weight",
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Weight is required';
-                            }
-                            return null;
-                          },
-                          surfixIcon: DropdownButton<String>(
-                            value: weightUnits,
-                            underline: Container(),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                weightUnits = newValue!;
-                              });
-                            },
-                            items: _weightUnitChoices
-                                .map((Map<String, String> pair) {
-                              return DropdownMenuItem<String>(
-                                value: pair["value"],
-                                child: Text(pair["label"]!),
-                              );
-                            }).toList(),
+                        ),AppCard(
+                          child: Column(
+                            children: [
+                              Icon(Icons.female),
+                              Text("Female"),
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: Constants.SPACING),
-                        FormInputTextField(
-                          keyboardType: TextInputType.number,
-                          controler: height,
-                          placeholder: "e.g 2",
-                          prefixIcon: Icons.height,
-                          label: "Height",
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Height is required';
-                            }
-                            return null;
-                          },
-                          surfixIcon: DropdownButton<String>(
-                            value: heightUnits,
-                            underline: Container(),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                heightUnits = newValue!;
-                              });
-                            },
-                            items: _heightUnitChoices
-                                .map((Map<String, String> pair) {
-                              return DropdownMenuItem<String>(
-                                value: pair["value"],
-                                child: Text(pair["label"]!),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        LinkedRichText(
-                          linked: "Your BMI:  ",
-                          unlinked: bmi != null ? bmi!.toStringAsFixed(1) : "_",
-                          unlinkedStyle:
-                              const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: Constants.SPACING),
-                        LinkedRichText(
-                          linked: "BMI status:  ",
-                          unlinked: bmi != null ? getBMIStatus(bmi!) : "_",
-                          unlinkedStyle:
-                              const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: Constants.SPACING),
-                        Button(
-                          title: "Calculate",
-                          onPress: handleSubmit,
-                        ),
-
-                        const SizedBox(
-                          height: Constants.SPACING,
                         ),
                       ],
                     ),
-                  ),
+                  ],
                 ),
               ),
             ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
---
+++ lib/src/shared/display/CustomeAppBar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nishauri/src/utils/constants.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  final IconData icon;
+  const CustomAppBar({super.key, required this.title, required this.subTitle, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final theme = Theme.of(context);
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: Constants.SPACING, vertical: Constants.SPACING * 3),
+      height: screenSize.height * 0.22,
+      decoration: BoxDecoration(
+        color: theme.colorScheme.primary,
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(Constants.SPACING),
+          bottomRight: Radius.circular(Constants.SPACING),
+        ),
+        image: const DecorationImage(
+          image: AssetImage("assets/images/contours.png"),
+          opacity: 0.1,
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(height: Constants.SPACING),
+          InkWell(
+            child: Icon(Icons.arrow_back, color: theme.canvasColor),
+            onTap: () => context.pop(),
+          ),
+          // IconButton(
+          //   onPressed: () => context.pop(),
+          //   icon: Icon(Icons.arrow_back, color: theme.canvasColor),
+          // ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: theme.textTheme.headlineLarge
+                    ?.copyWith(color: theme.canvasColor),
+              ),
+              const SizedBox(width: Constants.SPACING),
+              Icon(icon, color: theme.canvasColor,)
+            ],
+          ),
+          Text(
+            subTitle,
+            style: theme.textTheme.titleLarge
+                ?.copyWith(color: theme.canvasColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
