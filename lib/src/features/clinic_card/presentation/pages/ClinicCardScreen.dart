83f97b9d500392b1dca3ef4dcf8dcb1d3df927f5
Ogollah <kogollah2011@gmail.com>
2024-04-30 09:14:27 +0300

    consume clinic details endpoint

--- lib/src/features/clinic_card/presentation/pages/ClinicCardScreen.dart
+++ lib/src/features/clinic_card/presentation/pages/ClinicCardScreen.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nishauri/src/features/clinic_card/data/providers/programProvider.dart';
 import 'package:nishauri/src/features/clinic_card/presentation/widgets/clinicalDetails.dart';
 import 'package:nishauri/src/shared/display/CustomTabBar.dart';
 import 'package:nishauri/src/shared/display/CustomeAppBar.dart';
+import 'package:nishauri/src/utils/constants.dart';
 
 class ClinicCardScreen extends HookConsumerWidget {
   const ClinicCardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
     final programAsync = ref.watch(programProvider);
     final currIndex = useState(0);
     return programAsync.when(
@@ -49,8 +50,34 @@ class ClinicCardScreen extends HookConsumerWidget {
           ),
         );
       },
-      error: (error, _) => Center(child: Text(error.toString())),
-      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, _) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              error.toString(),
+              style: theme.textTheme.headlineSmall,
+            ),
+            const SizedBox(height: Constants.SPACING * 2),
+          ],
+        ),
+      ),
+      // Text(error.toString())),
+      loading: () => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Loading Programs",
+              style: theme.textTheme.headline6,
+            ),
+            const SizedBox(height: Constants.SPACING * 2),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
