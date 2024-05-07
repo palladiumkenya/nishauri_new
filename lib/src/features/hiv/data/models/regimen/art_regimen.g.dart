f7e59bc85958b9772f63a9b48eedd3922aaf07c6
Ogollah <kogollah2011@gmail.com>
2024-05-07 22:35:38 +0300

    Add regimen

--- lib/src/features/hiv/data/models/regimen/art_regimen.g.dart
+++ lib/src/features/hiv/data/models/regimen/art_regimen.g.dart
@@ -9,17 +9,17 @@ part of 'art_regimen.dart';
 _$ARTRegimenImpl _$$ARTRegimenImplFromJson(Map<String, dynamic> json) =>
     _$ARTRegimenImpl(
       id: json['id'] as String?,
-      name: json['name'] as String,
+      regimen: json['regimen'] as String,
       description: json['description'] as String?,
+      ccc_no: json['ccc_no'] as String?,
       isCurrent: json['isCurrent'] as bool? ?? false,
-      createdAt: json['createdAt'] as String,
     );
 
 Map<String, dynamic> _$$ARTRegimenImplToJson(_$ARTRegimenImpl instance) =>
     <String, dynamic>{
       'id': instance.id,
-      'name': instance.name,
+      'regimen': instance.regimen,
       'description': instance.description,
+      'ccc_no': instance.ccc_no,
       'isCurrent': instance.isCurrent,
-      'createdAt': instance.createdAt,
     };
