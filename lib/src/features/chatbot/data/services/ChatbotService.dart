2b185fdecc64bc497154f7063afdaa963bf30abd
Omoshlawi <lawiomosh3@gmail.com>
2024-05-29 09:55:24 +0300

    Chatbot feedback posting to server

--- lib/src/features/chatbot/data/services/ChatbotService.dart
+++ lib/src/features/chatbot/data/services/ChatbotService.dart
@@ -5,7 +5,7 @@ import 'package:nishauri/src/features/chatbot/data/models/message.dart';
 import 'package:nishauri/src/shared/interfaces/HTTPService.dart';
 import 'package:nishauri/src/utils/constants.dart';
 
-class ChatbotService extends HTTPService{
+class ChatbotService extends HTTPService {
   final String endpoint = "${Constants.BASE_URL}/chat";
 
   Future<FeedbackResponse> sendMessage(Message message) async {
@@ -15,7 +15,6 @@ class ChatbotService extends HTTPService{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-
         'question': message.question.toString(),
       }),
     );
@@ -25,5 +24,28 @@ class ChatbotService extends HTTPService{
     } else {
       throw Exception('Failed to send message');
     }
+  }
+
+  Future<String> review(Map<String, dynamic> data) async {
+    http.StreamedResponse response = await call(review_, data);
+    final responseString = await response.stream.bytesToString();
+    final jsonData = jsonDecode(responseString);
+    if (jsonData["success"] == true) {
+      return jsonData["msg"];
+    }
+    throw jsonData["msg"];
+  }
+
+  Future<http.StreamedResponse> review_(Map<String, dynamic> data) async {
+    final tokenPair = await getCachedToken();
+    var headers = {
+      'Authorization': "Bearer ${tokenPair.accessToken}",
+      'Content-Type': 'application/json',
+    };
+    var request =
+        http.Request('POST', Uri.parse('${Constants.BASE_URL_NEW}chat_review'));
+    request.body = jsonEncode(data);
+    request.headers.addAll(headers);
+    return await request.send();
   }
 }
