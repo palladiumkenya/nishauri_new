c60cbb564d04e6a723ec9fe68dc8124d70e4fde1
Omoshlawi <lawiomosh3@gmail.com>
2024-05-30 08:04:56 +0300

    Onboading screen iplemented and works greate, also updaed router t always start from onboarding

---
+++ lib/src/utils/data.dart
@@ -0,0 +1,34 @@
+final onboardingSteps = [
+  {
+    "id": "0",
+    "icon": "assets/images/schedule.svg",
+    "title": "Effortless Appointments",
+    "description":
+        "Easily reschedule your medical appointments at your convenience"
+  },
+  {
+    "id": "1",
+    "icon": "assets/images/laboratory.svg",
+    "title": "Instant Lab Results",
+    "description": "View your lab results as soon as they are available"
+  },
+  {
+    "id": "2",
+    "icon": "assets/images/medical_prescription.svg",
+    "title": "Manage Subscriptions",
+    "description": "Track your drug orders with ease using Dawa Drop"
+  },
+  {
+    "id": "3",
+    "icon": "assets/images/hospital_building.svg",
+    "title": "Find Nearby Facility",
+    "description": "Quickly locate hospitals and health facilities near you"
+  },
+  {
+    "id": "4",
+    "icon": "assets/images/chat_bot.svg",
+    "title": "Meet Nuru",
+    "description":
+        "Get instant assistance and answers t your health related questions"
+  },
+];
