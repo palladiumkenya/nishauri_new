caa320153a7ffa3bb319c758cb7afc2a555fcce3
Omoshlawi <lawiomosh3@gmail.com>
2024-06-03 09:29:30 +0300

    Showing onboarding screen only on first time install
    fix

--- lib/src/features/user_preference/presentation/controlers/SettingsControler.dart
+++ lib/src/features/user_preference/presentation/controlers/SettingsControler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nishauri/src/features/user_preference/data/models/Setings.dart';
 import 'package:nishauri/src/local_storage/LocalStorage.dart';
