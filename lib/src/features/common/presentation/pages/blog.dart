bda15a843e50640ffd98d48b559ff54ca7069c90
Ogollah <kogollah2011@gmail.com>
2024-06-05 14:54:18 +0300

    update blog

--- lib/src/features/common/presentation/pages/blog.dart
+++ lib/src/features/common/presentation/pages/blog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:nishauri/src/features/common/data/models/announcement.dart';
+import 'package:nishauri/src/shared/display/CustomeAppBar.dart';
 import 'package:nishauri/src/shared/display/scafold_stack_body.dart';
 import 'package:nishauri/src/utils/constants.dart';
 import 'package:nishauri/src/utils/helpers.dart';
@@ -14,25 +15,35 @@ class BlogPostScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      body: ScaffoldStackedBody(
-        body: Padding(
-          padding: const EdgeInsets.all(Constants.SPACING),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppBar(backgroundColor: Colors.transparent),
-              Text(announcement.title, style: theme.textTheme.headlineMedium),
-              Image.network(
-                announcement.image,
-                width: double.infinity,
-                height: getOrientationAwareScreenSize(context).height * 0.25,
-                fit: BoxFit.cover,
+      body: Column(
+        children: [
+          CustomAppBar(
+            title: "Did you know",
+            icon: Icons.question_answer_outlined,
+            color: Constants.programsColor.withOpacity(0.5),),
+
+          Expanded(
+            child:       ScaffoldStackedBody(
+              body: Padding(
+                padding: const EdgeInsets.all(Constants.SPACING),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(announcement.title, style: theme.textTheme.headlineMedium),
+                    Image.network(
+                      announcement.image,
+                      width: double.infinity,
+                      height: getOrientationAwareScreenSize(context).height * 0.30,
+                      fit: BoxFit.cover,
+                    ),
+                    Expanded(child: Markdown(data: announcement.description!))
+                  ],
+                ),
               ),
-              Expanded(child: Markdown(data: announcement.description!))
-            ],
-          ),
-        ),
-      ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
