308c1948a2873b0cfde4d3e5c8d33c0038192047
Ogollah <kogollah2011@gmail.com>
2024-07-03 11:10:39 +0300

    Fix viral load date color on dark mode and add idle time logout

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nishauri/src/app/app_theme.dart';
 import 'package:nishauri/src/app/navigation/app_router.dart';
+import 'package:nishauri/src/features/auth/data/providers/auth_provider.dart';
 
 class NishauriApp extends StatelessWidget {
   const NishauriApp({super.key});
@@ -11,16 +12,16 @@ class NishauriApp extends StatelessWidget {
     return Consumer(
       builder: (context, ref, child) {
         final router = ref.watch(routesProvider);
+        final authRepository = ref.watch(authStateProvider);
+        final userRepository = ref.watch(authStateProvider);
+
         return MaterialApp.router(
           title: "Nishauri",
-          // routeInformationParser: router.routeInformationParser,
-          // routerDelegate: router.routerDelegate,
-          // routeInformationProvider: router.routeInformationProvider,
           routerConfig: router,
           theme: ref.watch(mainTheme),
           debugShowCheckedModeBanner: false,
         );
-      }
+      },
     );
   }
 }
