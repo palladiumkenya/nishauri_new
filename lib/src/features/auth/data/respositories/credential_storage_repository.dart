0624100cf8ab56d883d57d93d7c479f79e0bf664
NewtonMutugi <newtonmut197@gmail.com>
2024-07-22 11:43:42 +0300

    Implemeneted Biometric login functionality on LoginScreen

--- lib/src/features/auth/data/respositories/credential_storage_repository.dart
+++ lib/src/features/auth/data/respositories/credential_storage_repository.dart
@@ -3,16 +3,16 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class CredentialStorageRepository {
   final _storage = const FlutterSecureStorage();
 
-  Future<void> saveCredentials(String phone_number, String password) async {
-    await _storage.write(key: 'phone_number', value: phone_number);
+  Future<void> saveCredentials(String username, String password) async {
+    await _storage.write(key: 'username', value: username);
     await _storage.write(key: 'password', value: password);
   }
 
   Future<Map<String, String>> fetchCredentials() async {
-    String? phone_number = await _storage.read(key: 'phone_number');
+    String? username = await _storage.read(key: 'username');
     String? password = await _storage.read(key: 'password');
     return {
-      'phone_number': phone_number ?? '',
+      'username': username ?? '',
       'password': password ?? '',
     };
   }
