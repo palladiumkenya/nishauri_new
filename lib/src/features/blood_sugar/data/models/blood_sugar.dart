a70bd724fb334b3ac8ff79267e2b3979897b6c12
NewtonMutugi <newtonmut197@gmail.com>
2024-07-25 17:53:17 +0300

    Add BloodSugar model for tracking blood sugar levels

---
+++ lib/src/features/blood_sugar/data/models/blood_sugar.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'blood_sugar.freezed.dart';
+part 'blood_sugar.g.dart';
+
+@freezed
+class BloodSugar with _$BloodSugar {
+  const factory BloodSugar({
+    required int id,
+    required double level,
+    required DateTime timestamp,
+  }) = _BloodSugar;
+
+  factory BloodSugar.fromJson(Map<String, dynamic> json) =>
+      _$BloodSugarFromJson(json);
+}
---
+++ lib/src/features/blood_sugar/data/models/blood_sugar.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'blood_sugar.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$BloodSugarImpl _$$BloodSugarImplFromJson(Map<String, dynamic> json) =>
+    _$BloodSugarImpl(
+      id: (json['id'] as num).toInt(),
+      level: (json['level'] as num).toDouble(),
+      timestamp: DateTime.parse(json['timestamp'] as String),
+    );
+
+Map<String, dynamic> _$$BloodSugarImplToJson(_$BloodSugarImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'level': instance.level,
+      'timestamp': instance.timestamp.toIso8601String(),
+    };
