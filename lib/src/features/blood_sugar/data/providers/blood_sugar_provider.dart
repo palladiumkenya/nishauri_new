4760cf0a2f41cdea94bd73e425284f92e55bcfa7
NewtonMutugi <newtonmut197@gmail.com>
2024-08-02 02:24:52 +0300

    Update blood sugar provider to use blood sugar service for saving and fetching data

--- lib/src/features/blood_sugar/data/providers/blood_sugar_provider.dart
+++ lib/src/features/blood_sugar/data/providers/blood_sugar_provider.dart
@@ -1,28 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nishauri/src/features/blood_sugar/data/models/blood_sugar.dart';
 import 'package:nishauri/src/features/blood_sugar/data/repository/blood_sugar_repository.dart';
-
-class BloodSugarNotifier extends StateNotifier<List<BloodSugar>> {
-  final BloodSugarRepository repository;
-
-  BloodSugarNotifier(this.repository) : super(repository.getAllEntries());
-
-  void addEntry(BloodSugar entry) {
-    repository.addEntry(entry);
-    state = [...state, entry];
-  }
-
-  void refreshEntries() {
-    state = repository.getAllEntries();
-  }
-}
+import 'package:nishauri/src/features/blood_sugar/data/services/blood_sugar_service.dart';
 
 final bloodSugarProvider = Provider<BloodSugarRepository>((ref) {
-  return BloodSugarRepository();
+  return BloodSugarRepository(BloodSugarService());
 });
 
-final bloodSugarEntriesProvider =
-    StateNotifierProvider<BloodSugarNotifier, List<BloodSugar>>((ref) {
+final bloodSugarEntriesProvider = FutureProvider<List<BloodSugar>>((ref) async {
   final repository = ref.watch(bloodSugarProvider);
-  return BloodSugarNotifier(repository);
+  return await repository.getBloodSugars();
 });
