3403811461bf5351c1ae16f2ed19293eab609359
NewtonMutugi <newtonmut197@gmail.com>
2024-08-02 03:20:59 +0300

    Refactor BloodSugarRepository to use BloodSugarService for saving and fetching blood sugar data

--- lib/src/features/blood_sugar/data/repository/blood_sugar_repository.dart
+++ lib/src/features/blood_sugar/data/repository/blood_sugar_repository.dart
@@ -1,10 +1,5 @@
-import 'dart:convert';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
-import 'package:http/http.dart';
 import 'package:nishauri/src/features/blood_sugar/data/models/blood_sugar.dart';
 import 'package:nishauri/src/features/blood_sugar/data/services/blood_sugar_service.dart';
-import 'package:nishauri/src/utils/constants.dart';
 
 class BloodSugarRepository {
   final BloodSugarService _service;
