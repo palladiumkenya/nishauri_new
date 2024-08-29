626d619c622b094f9e9816d5397ead0f89c19de9
Eugene600 <ugowach160@gmail.com>
2024-08-29 19:05:24 +0300

    Grouping the cycles by their years and making sure the latest cycle is displayed first

--- lib/src/features/period_planner/presentation/pages/periods_history.dart
+++ lib/src/features/period_planner/presentation/pages/periods_history.dart
@@ -5,7 +5,6 @@ import 'package:nishauri/src/features/period_planner/presentation/widgets/custom
 import 'package:nishauri/src/shared/display/CustomeAppBar.dart';
 import 'package:nishauri/src/utils/constants.dart';
 
-
 class PeriodsHistory extends StatefulWidget {
   const PeriodsHistory({super.key});
 
@@ -16,36 +15,31 @@ class PeriodsHistory extends StatefulWidget {
 class _PeriodsHistoryState extends State<PeriodsHistory> {
   final averagePeriod = calculateAveragePeriodLength(cycles);
   final averageCycles = calculateAverageCycleLength(cycles);
-  // final List<Map<String, String>> periodHistory = [
-  // {'start': 'July 1', 'end': 'July 7'},
-  // {'start': 'August 1', 'end': 'August 7'},
-  // {'start': 'September 2', 'end': 'September 8'},
-  // {'start': 'October 3', 'end': 'October 9'},
-  // {'start': 'November 4', 'end': 'November 10'},
-  // {'start': 'December 5', 'end': 'December 11'},
-  // {'start': 'January 6', 'end': 'January 12'},
-  // {'start': 'February 7', 'end': 'February 13'},
-  // {'start': 'March 8', 'end': 'March 14'},
-  // {'start': 'April 9', 'end': 'April 15'},
-  // {'start': 'May 10', 'end': 'May 16'},
-  // {'start': 'June 11', 'end': 'June 17'},
-  // {'start': 'July 12', 'end': 'July 18'},
-  // {'start': 'August 13', 'end': 'August 19'},
-  // {'start': 'September 14', 'end': 'September 20'},
-  // {'start': 'October 15', 'end': 'October 21'},
-  // {'start': 'November 16', 'end': 'November 22'},
-  // {'start': 'December 17', 'end': 'December 23'},
-  // {'start': 'January 18', 'end': 'January 24'},
-  // {'start': 'February 19', 'end': 'February 25'},
-  // ];
 
   // Function to format the date as "MMM d"
   String formatDate(DateTime date) {
     return DateFormat('MMM d').format(date);
   }
 
+  // Function to group cycles by year
+  Map<int, List<Cycle>> groupCyclesByYear(List<Cycle> cycles) {
+    final Map<int, List<Cycle>> groupedCycles = {};
+    for (var cycle in cycles) {
+      final year = cycle.periodStart.year;
+      if (!groupedCycles.containsKey(year)) {
+        groupedCycles[year] = [];
+      }
+      groupedCycles[year]!.add(cycle);
+    }
+    return groupedCycles;
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Group cycles by year and reverse the list to show the latest first
+    final groupedCycles = groupCyclesByYear(cycles);
+    final sortedYears = groupedCycles.keys.toList()..sort((a, b) => b.compareTo(a));
+
     return Scaffold(
       body: ListView(
         children: [
@@ -54,150 +48,176 @@ class _PeriodsHistoryState extends State<PeriodsHistory> {
             color: Constants.periodPlanner,
           ),
           const SizedBox(height: 10),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    //The first Card showing both Average period and cycle lengths
-                    Card(
-                      elevation: 4.0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              "My Cycles",
-                              style:  TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  //Card showing Average Period days 
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12.0),
-                                    ),
-                                    child: Padding(
-                                      padding:  const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        children: [
-                                          const Text(
-                                            "Average Period",
-                                            style: TextStyle(
-                                              fontSize: 18.0,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 8.0),
-                                          Text(
-                                            "$averagePeriod days",
-                                            style: const TextStyle(
-                                              fontSize: 30.0,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.pink,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  //Card Showing Average Cycle days
-                                  child: Card(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12.0),
-                                    ),
-                                    child: Padding(
-                                      padding:  const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        children: [
-                                          const Text(
-                                            'Average Cycle',
-                                            style: TextStyle(
-                                              fontSize: 20.0,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          const SizedBox(height: 8.0),
-                                          Text(
-                                            "$averageCycles days",
-                                            style: const TextStyle(
-                                              fontSize: 30.0,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.blue,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10)
-                          ],
-                        ),
-                      ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  // The first Card showing both Average period and cycle lengths
+                  Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
-                    const SizedBox(height: 20),
-                    //Card Showing period history
-                    Card(
-                      elevation: 4.0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-                        child: Column(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "My Cycles",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                            const Text(
-                              "History",
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.pink
+                              Expanded(
+                                // Card showing Average Period days
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        const Text(
+                                          "Average Period",
+                                          style: TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 8.0),
+                                        Text(
+                                          "$averagePeriod days",
+                                          style: const TextStyle(
+                                            fontSize: 30.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.pink,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
-                            ListView.builder(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: cycles.length,
-                              itemBuilder: (context, index) {
-                                final cycle = cycles[index];
-                                final start = formatDate(cycle.periodStart);
-                                final end = formatDate(cycle.periodEnd);
-                                return ListTile(
-                                  title: const Text("2024"),
-                                  subtitle: Text(
-                                    "$start - $end",
-                                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                              Expanded(
+                                // Card Showing Average Cycle days
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
                                   ),
-                                  trailing: IconButton(
-                                    onPressed: () {}, 
-                                    icon: const Icon(Icons.arrow_forward_ios),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        const Text(
+                                          'Average Cycle',
+                                          style: TextStyle(
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 8.0),
+                                        Text(
+                                          "$averageCycles days",
+                                          style: const TextStyle(
+                                            fontSize: 30.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                );
-                              }
-                            ),
-                          ],
-                        ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Card Showing period history
+                  Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "History",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink,
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: sortedYears.length,
+                            itemBuilder: (context, index) {
+                              final year = sortedYears[index];
+                              final cyclesInYear = groupedCycles[year]!;
+                              
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                    child: Text(
+                                      "$year",
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  ...cyclesInYear.reversed.map((cycle) {
+                                    final start = formatDate(cycle.periodStart);
+                                    final end = formatDate(cycle.periodEnd);
+                                    final cycleDays = cycle.cycleLength;
+                                    return ListTile(
+                                      title: Text(
+                                        "$start - $end",
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      subtitle: Text(
+                                        "Length of cycle: $cycleDays days",
+                                        style: const TextStyle(
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                      trailing: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.arrow_forward_ios),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
