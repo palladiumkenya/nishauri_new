1e5482c2cb3881c2c7e9283e9d07e05066f54c0b
Eugene600 <ugowach160@gmail.com>
2024-09-02 15:51:05 +0300

    Allowing users to edit their periods however there are some issues to solve since screens are not updating

--- lib/src/features/period_planner/presentation/pages/editPeriodsScreen.dart
+++ lib/src/features/period_planner/presentation/pages/editPeriodsScreen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nishauri/src/features/period_planner/data/models/cycle.dart';
+import 'package:nishauri/src/features/period_planner/presentation/widgets/customCalendar.dart';
 import 'package:nishauri/src/shared/display/CustomeAppBar.dart';
 import 'package:nishauri/src/utils/constants.dart';
+import 'package:nishauri/src/utils/routes.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class EditPeriods extends StatefulWidget {
   final DateTime? initialStartDate;
   final DateTime? initialEndDate;
-  const EditPeriods({super.key, this.initialStartDate, this.initialEndDate});
+  const EditPeriods(
+      {super.key, this.initialStartDate, this.initialEndDate});
 
   @override
   State<EditPeriods> createState() => _EditPeriodsState();
@@ -17,17 +22,50 @@ class _EditPeriodsState extends State<EditPeriods> {
   DateTime? _rangeStart;
   DateTime? _rangeEnd;
 
-   @override
+  @override
   void initState() {
     super.initState();
-     
+
     _focusedDay = widget.initialStartDate ?? DateTime.now();
     _rangeStart = widget.initialStartDate;
     _rangeEnd = widget.initialEndDate;
   }
 
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime? focusedDay) {
+    setState(() {
+      _rangeStart = start;
+      _rangeEnd = end;
+      _focusedDay = focusedDay ?? _focusedDay;
+    });
+  }
+
+  //Function for updating the entries in the list database
+  void _updateCycle() {
+    if (_rangeStart != null && _rangeEnd != null) {
+      //Finding the current cycle to update
+      Cycle currentCycle = cycles.firstWhere(
+        (cycle) =>
+            cycle.periodStart == widget.initialStartDate &&
+            cycle.periodEnd == widget.initialEndDate,
+      );
+
+      // Predict the new cycle based on the updated period start and end dates
+      Cycle updatedCycle = predictCycle(_rangeStart!, _rangeEnd!);
+
+      //Replace the old cycle with the updated one
+      int index = cycles.indexOf(currentCycle);
+      cycles[index] = updatedCycle;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Periods updated successfully!')),
+      );
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       body: Column(
         children: [
@@ -41,13 +79,8 @@ class _EditPeriodsState extends State<EditPeriods> {
             lastDay: DateTime(2100),
             rangeStartDay: _rangeStart,
             rangeEndDay: _rangeEnd,
-            onRangeSelected: (start, end, focusedDay) {
-              setState(() {
-                _rangeStart = start;
-                _rangeEnd = end;
-                _focusedDay = focusedDay;
-              });
-            },
+            rangeSelectionMode: RangeSelectionMode.toggledOn,
+            onRangeSelected: _onRangeSelected,
             calendarStyle: const CalendarStyle(
               todayDecoration: BoxDecoration(
                 color: Colors.blue,
@@ -69,6 +102,57 @@ class _EditPeriodsState extends State<EditPeriods> {
             ),
             headerStyle: const HeaderStyle(
               formatButtonVisible: false,
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Constants.periodPlanner,
+                  ),
+                  onPressed: () {
+                    // if (_startDate != null) {
+                    //   final endDate = _endDate ?? _startDate!; // The else statement handles where a period only happens for a single day hence the end date will be same day as start date
+                    //   _updateOrAddCycle(_startDate!, endDate);
+                    //   printCycles(cycles);
+                    //   context.goNamed(RouteNames.PERIOD_PLANNER_SCREEN);
+                    // } else {
+                    //   ScaffoldMessenger.of(context).showSnackBar(
+                    //   const SnackBar(content: Text('Please select your Period start and end dates.')),
+                    //   );
+                    // }
+                    if (_rangeStart == null || _rangeEnd == null) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Invalid Selection'),
+                          content: const Text('Please select both start and end of your periods!!'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      _updateCycle();
+                      context.goNamed(RouteNames.PERIOD_PLANNER_PERIOD_HISTORY);
+                    }
+                  },
+                  child: Text(
+                    'Apply',
+                    style: theme.textTheme.titleSmall?.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
